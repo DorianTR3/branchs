472393f9147b2f1c31eee834f7d8e01c63b84d2d
luis dorian <wiwish2016@hotmail.com>
2024-02-24 22:47:53 -0600

    branchs

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int v=4,x=7, suma;
+        suma=v+x;
+        System.out.println(suma);
+        System.out.println("resta: "+(v-x));
+
 
     }
 }
